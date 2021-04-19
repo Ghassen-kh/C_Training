e9c2fa32c23381080451edc879b8ef7228c6987a
ghassen <ghassen.khalouaoui15@gmail.com>
2021-04-19 23:15:50 +0100

    pointer array modification

--- pointer_array.c
+++ pointer_array.c
@@ -1,13 +1,15 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+const int MAX = 3;
 
-int main (int argc, char **argv){
-    int a [10], *iptr;
-    iptr=a;
-    *iptr=5;
-    *(iptr+1)=7;
-    printf("la valeur de a[0]= %d\n",a[0]);
-    printf("la valeur de a[1]= %d\n",a[1]);
-
-    return 0;     
+int main (){
+    int var[]={10,100,200};
+    int i, * ptr [MAX];
+    for (i=0;i<MAX;i++){
+        printf("The value of var[%d] = %d\n",i , var[i]);
+        ptr[i]=&var[i]; /* Assign the address of integer */
+    }
+    for (i=0;i<MAX;i++){
+        printf("The value of var[%d] = %p\n",i,*ptr[i]);
+    }
+    return 0;
 }
